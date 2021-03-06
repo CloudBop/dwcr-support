06c4d34edf2be3eb463e10887e72d55b27d7fe60
Cloudbop <boppincolin@gmail.com>
2021-03-06 22:11:35 +0000

    setup template for frontpage/content-hero

--- page-home.php
+++ page-home.php
@@ -1,18 +1,6 @@
-<?php
-/*
-	Template Name: Home Page	
-*/
+<?php /* Template Name: Page Front */ 
 
 get_header(); ?>
 
-	<?php get_template_part('frontpage-content','hero'); ?>
-	<?php get_template_part('frontpage-content','optin'); ?>
-	<?php get_template_part('frontpage-content','boost'); ?>
-	<?php get_template_part('frontpage-content','benefits'); ?>
-	<?php get_template_part('frontpage-content','coursefeatures'); ?>
-	<?php get_template_part('frontpage-content','projectfeatures'); ?>
-	<?php get_template_part('frontpage-content','featurette'); ?>
-	<?php get_template_part('frontpage-content','instructor'); ?>
-	<?php get_template_part('frontpage-content','testimonials'); ?>
-
+<!-- Nothing to see here yet -->
 <?php get_footer(); ?>
