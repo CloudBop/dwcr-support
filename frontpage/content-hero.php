c1e3478bf9dfb627d8e385b4c312a0a62c6ab350
Cloudbop <boppincolin@gmail.com>
2021-03-06 22:11:46 +0000

    Fixed hardcoded image

---
+++ frontpage/content-hero.php
@@ -0,0 +1,25 @@
+
+<!-- TODO ADD PHP VAR FROM ACF -->
+
+<section class="header1 top-section-hero-image mbr-fullscreen" id="header1-1">
+  <div class="mbr-overlay" style="opacity: 0.5; background-color: rgb(0, 0, 0);"></div>
+
+  <!-- <img src="<?php echo $income_feature_image['url']; ?>" alt="<?php echo $income_feature_image['alt']; ?>"> -->
+  <img src="<?php bloginfo('stylesheet_directory')?>/assets/src/img/theme/image/mbr.jpg">
+  
+  
+  <div class="container">
+      <div class="row">
+          <div class="col-12 col-lg-6">
+              <h1 class="mbr-section-title mbr-fonts-style mb-3 display-1"><strong>Rare Together</strong></h1>
+
+              <p class="mbr-text mbr-fonts-style display-7">
+                  We are a community of people living with PNH (and their family members) in England, Wales and
+                  Northern Ireland supporting one other, sharing our experiences and engaging together with the
+                  stakeholders in our world.</p>
+              <div class="mbr-section-btn mt-3"><a class="btn btn-secondary display-4" href="#">Learn More</a>
+              </div>
+          </div>
+      </div>
+  </div>
+</section>
