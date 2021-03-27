50ee87c3d4cb1782da480bfe5bbe0912b09cf7c9
Cloudbop <boppincolin@gmail.com>
2021-03-27 13:57:24 +0000

    simplified frontpage template
    Co-authored-by: David <DW-87@users.noreply.github.com>

--- front-page.php
+++ front-page.php
@@ -6,11 +6,12 @@ get_header(); ?>
 
 <!-- Nothing to see here yet -->
 
-<div class="container-fluid">
+<div class="not-in-use-main-wrapper">
     <?php 
       // 
-        get_template_part('template-parts/frontpage/frontpage','section1'); 
-        get_template_part('template-parts/frontpage/frontpage','section2'); 
+        // get_template_part('template-parts/frontpage/frontpage','section1'); 
+        // get_template_part('template-parts/frontpage/frontpage','section2'); 
+        get_template_part('template-parts/frontpage/frontpage','section3'); 
       ?>
 	</div>
 <?php get_footer(); ?>
