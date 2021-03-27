1ee28fda089ce21824e512d8bdef39f5bf077d52
Cloudbop <boppincolin@gmail.com>
2021-03-27 18:43:58 +0000

    +static frontpage sections

--- front-page.php
+++ front-page.php
@@ -5,7 +5,9 @@
 get_header(); ?>
 <div class="not-in-use-main-wrapper">
   <?php 
-    get_template_part('template-parts/frontpage/frontpage','section1'); 
+    get_template_part('template-parts/frontpage/frontpage','section1');
+    get_template_part('template-parts/frontpage/frontpage','section2');
+    get_template_part('template-parts/frontpage/frontpage','section3');
 
   ?>
 </div>
