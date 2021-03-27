d877e4c4bfe4d8c07643089ecc3d70db2db47605
Cloudbop <boppincolin@gmail.com>
2021-03-27 14:23:49 +0000

    neatend: see diff

--- front-page.php
+++ front-page.php
@@ -1,17 +1,12 @@
 <?php 
 /** the frontpage template - when in wp admin area as static frontpage 
+ * 
  * */ 
-
 get_header(); ?>
-
-<!-- Nothing to see here yet -->
-
 <div class="not-in-use-main-wrapper">
-    <?php 
-      // 
-        // get_template_part('template-parts/frontpage/frontpage','section1'); 
-        // get_template_part('template-parts/frontpage/frontpage','section2'); 
-        get_template_part('template-parts/frontpage/frontpage','section3'); 
-      ?>
-	</div>
+  <?php 
+    get_template_part('template-parts/frontpage/frontpage','section1'); 
+
+  ?>
+</div>
 <?php get_footer(); ?>
