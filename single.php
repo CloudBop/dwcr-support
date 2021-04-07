67842844c44147adaf901b930c660d1e17499f6c
Cloudbop <boppincolin@gmail.com>
2021-04-07 23:57:31 +0100

    +single post template

---
+++ single.php
@@ -0,0 +1,12 @@
+<?php get_header(); 
+// render single-post, ie NOT page template 
+?>
+
+<div class="container">
+    
+    Post template
+    
+</div>
+<?php get_footer(); ?> 
+<?php //get_template_part( 'loop', 'page' ); ?>
+<?php  //get_sidebar(); ?>
