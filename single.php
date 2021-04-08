624bbe0a1c02877de8ad3e757f3a44a5fe64e07c
Cloudbop <boppincolin@gmail.com>
2021-04-08 12:37:28 +0100

    + single post template

--- single.php
+++ single.php
@@ -2,10 +2,19 @@
 // render single-post, ie NOT page template 
 ?>
 
-<div class="container">
-    
-    Post template
-    
+<div class="container danger">
+    <div class="row">
+        <main class="col-sm-8 main-content">
+            
+            <?php get_template_part( 'wp-loop/loop', 'single' ); ?>
+        </main>
+        
+        <aside class="col-sm-4 sidebar-content danger">
+              <?php dynamic_sidebar('sidebar-1'); ?>
+              This is where the sidebar goes, no registered sidebars as of yet.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci veritatis cupiditate cumque ad quidem placeat! A doloribus nobis nostrum fugit quidem aliquam error assumenda, harum dolore totam necessitatibus placeat? Officiis?
+        </aside>
+    </div>
 </div>
 <?php get_footer(); ?> 
 <?php //get_template_part( 'loop', 'page' ); ?>
