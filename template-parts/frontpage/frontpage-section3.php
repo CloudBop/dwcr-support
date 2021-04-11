98099ae83a2e8834eb1d064dcd08f3f4021477b8
Cloudbop <boppincolin@gmail.com>
2021-04-11 18:01:42 +0100

    neatened section3 styles mkup

--- template-parts/frontpage/frontpage-section3.php
+++ template-parts/frontpage/frontpage-section3.php
@@ -1,18 +1,18 @@
 <!-- Start PNH What Is Section -->
 <section id="what-is-section">
   <div class="container">
-    <div class="row">
-      <div class="col text-center">
-        <h1 class="display-4">
+    
+      <div class=" text-center">
+        <h2 class="">
           What Is Paroxysmal Nocturnal Haemoglobinuria (PNH)?
-        </h1>
-        <p class="lead">
+        </h2>
+        <p class="">
           PNH is a rare blood disease where PNH affected blood cells are
           attacked by the complement part of the body’s immune system.
         </p>
         <button class="btn btn-lg pnh-red-button">Read More</button>
       </div>
-    </div>
+    
   </div>
 </section>
 <!-- End PNH What Is Section -->
