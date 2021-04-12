8c4294b4b7df20d2c460745cf02e55a278d1e000
Cloudbop <boppincolin@gmail.com>
2021-04-12 21:34:29 +0100

    mvd loop into page.php, simpler templating

--- page.php
+++ page.php
@@ -5,8 +5,34 @@
 <div>
     
         <main class="main-content">
-          <?php get_template_part('template-parts/components/page/entry-header'); ?>
-          <?php get_template_part( 'wp-loop/loop', 'page' ); ?>
+        <?php get_template_part('template-parts/components/page/entry-header'); ?>
+
+
+
+        <div class="container">
+          <div class="row">
+          <?php if(have_posts()) { ?>
+            <?php while(have_posts()) { ?>
+              <?php the_post(); ?>
+                  <div class="col">
+                    <?php //get_template_part('template-parts/components/page/entry-header'); ?>
+                    <?php get_template_part('template-parts/components/page/entry-content'); ?>
+                    <?php get_template_part('template-parts/components/page/entry-footer'); ?>
+                  </div>
+                <?php 
+                // if (comments_open() || get_comments_number() ) {
+                //     comments_template();
+                // }
+                ?>
+            <?php } // end while?>
+          <?php } else {?>
+
+              <div class="col">
+                <?php get_template_part('template-parts/post/content','none'); ?>
+              </div>
+          <?php }  // end if else ?>
+          </div>
+        </div>
         </main>
 
         <?php /* 
@@ -15,7 +41,6 @@
               This is where the sidebar goes, no registered sidebars as of yet.
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci veritatis cupiditate cumque ad quidem placeat! A doloribus nobis nostrum fugit quidem aliquam error assumenda, harum dolore totam necessitatibus placeat? Officiis?
         </aside> ?/*/?>
-    
 </div>
 
 <?php get_footer(); ?> 
