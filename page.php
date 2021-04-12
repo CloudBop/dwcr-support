5263b512e4258d1253fd8747f4d8ba91b3f9082e
Cloudbop <boppincolin@gmail.com>
2021-04-12 21:49:35 +0100

    neatened page.php

--- page.php
+++ page.php
@@ -2,38 +2,33 @@
 // render Page, ie NOT post 
 ?>
 
-<div>
-    
-        <main class="main-content">
-        <?php get_template_part('template-parts/components/page/entry-header'); ?>
+<main class="main-content">
+  <?php get_template_part('template-parts/components/page/entry-header'); ?>
+  <div class="container">
+    <div class="row">
+    <?php if(have_posts()) { ?>
+      <?php while(have_posts()) { ?>
+        <?php the_post(); ?>
+            <div class="col">
+              <?php //get_template_part('template-parts/components/page/entry-header'); ?>
+              <?php get_template_part('template-parts/components/page/entry-content'); ?>
+              <?php get_template_part('template-parts/components/page/entry-footer'); ?>
+            </div>
+          <?php 
+          // if (comments_open() || get_comments_number() ) {
+          //     comments_template();
+          // }
+          ?>
+      <?php } // end while?>
+    <?php } else {?>
 
-
-
-        <div class="container">
-          <div class="row">
-          <?php if(have_posts()) { ?>
-            <?php while(have_posts()) { ?>
-              <?php the_post(); ?>
-                  <div class="col">
-                    <?php //get_template_part('template-parts/components/page/entry-header'); ?>
-                    <?php get_template_part('template-parts/components/page/entry-content'); ?>
-                    <?php get_template_part('template-parts/components/page/entry-footer'); ?>
-                  </div>
-                <?php 
-                // if (comments_open() || get_comments_number() ) {
-                //     comments_template();
-                // }
-                ?>
-            <?php } // end while?>
-          <?php } else {?>
-
-              <div class="col">
-                <?php get_template_part('template-parts/post/content','none'); ?>
-              </div>
-          <?php }  // end if else ?>
-          </div>
+        <div class="col">
+          <?php get_template_part('template-parts/post/content','none'); ?>
         </div>
-        </main>
+    <?php }  // end if else ?>
+    </div>
+  </div>
+</main>
 
         <?php /* 
         <aside class="col-sm-4 sidebar-content danger">
