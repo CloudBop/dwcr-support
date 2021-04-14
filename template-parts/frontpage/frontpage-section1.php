3ca53534b505873f1ec683b489087534576401e4
Cloudbop <boppincolin@gmail.com>
2021-04-14 01:55:48 +0100

    neatened section 1 +landing typography

--- template-parts/frontpage/frontpage-section1.php
+++ template-parts/frontpage/frontpage-section1.php
@@ -1,17 +1,20 @@
 <?php
 // DATA FROM ACF  - https://support.advancedcustomfields.com/forums/topic/how-to-use-the-image-field-for-backgrounds/
 $section1_image	= get_field('section1_image');
+
+// Todo
+/* <img class="img-fluid" src="<?php echo $section1_image['url']; ?>" alt="<?php echo $section1_image['alt']; ?>">*/
 ?>
 <!-- Start Hero Section -->
 <section class="col-12 p-0">
-  <?php //TODO?  <!-- <img class="img-fluid" src="" alt=""> --> ?>
+ 
   <header id="hero-section" style="background-image: url(<?php the_field('section1_image'); ?>);">
     <div class="dark-overlay">
       <!-- main content -->
       <div class="hero-inner container d-flex">
         <div class="row align-self-center">
           <div class="col-md-6">
-            <h1 class="display-4">
+            <h1>
               <?php the_field('section1_title'); ?>
             </h1>
             <p>
