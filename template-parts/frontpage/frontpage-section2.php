09f2b4c496a4498ea0f38f9d772eeff925cabf82
Cloudbop <boppincolin@gmail.com>
2021-04-14 01:57:05 +0100

    +section2 see diff

--- template-parts/frontpage/frontpage-section2.php
+++ template-parts/frontpage/frontpage-section2.php
@@ -6,14 +6,14 @@ $section2_image	= get_field('section2_image');
 <section id="welcome-section" class="py-5">
   <div class="container">
     <div class="row">
-      <div class="col-md-6 align-self-center welcome-breakpoint">
+      <div class="col-md-6 align-self-center mb-5 mb-md-0">
         <h2><?php the_field('section2_title'); ?></h2>
         <p class="">
           <?php the_field('section2_blurb'); ?>
         </p>
         <button class="btn btn-lg btn-danger">  <?php the_field('section2_cta'); ?></button>
       </div>
-      <div class="col-md-6">
+      <div class="col-md-6 d-flex">
          <?php // WP doesn't allow SVG... hardcoded asset the_field('section2_cta'); ?>
         <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/src/img/svg/pnh-uk-map.svg" alt="" class="img-fluid mb-3 welcome-image" />
       </div>
