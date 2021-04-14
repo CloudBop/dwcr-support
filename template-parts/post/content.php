455eee93c4a9db835c94d5f18fd02797484dcf28
Cloudbop <boppincolin@gmail.com>
2021-04-14 17:56:26 +0100

    +content neatening

--- template-parts/post/content.php
+++ template-parts/post/content.php
@@ -1,35 +1,33 @@
-<article <?php echo post_class('c-post u-margin-bottom-20'); ?> >
+<article <?php echo post_class(); ?> >
 
-    <div class="c-post__inner">
+    <?php if(get_the_post_thumbnail() !== '') { ?>
+        <div class="c-post__thumbnail">
+            <?php 
+            // TODO - configure thumbnails for correct sizing
+            the_post_thumbnail( 'pnhuk-theme-blog-image' ); ?>
+        </div>
+    <?php } ?>
 
-        <?php if(get_the_post_thumbnail() !== '') { ?>
-            <div class="c-post__thumbnail">
-                <?php the_post_thumbnail( '_themename-blog-image' ); ?>
-            </div>
-        <?php } ?>
-
-        <?php get_template_part('template-parts/post/header') ?>
+    <?php // get_template_part('template-parts/post/header') ?>
+    
+    
+    <?php if(is_single( )) { ?>
         
-        <?php if(is_single( )) { ?>
-            <div class="c-post__content">
-                <?php the_content(); 
-                wp_link_pages();
-                ?>
-            </div>
-        <?php } else { ?>
-            <div class="c-post__excerpt">
-                <?php the_excerpt(); ?>
-            </div>
-        <?php } ?>
+            <?php the_content(); 
+            wp_link_pages();
+            ?>
+        
+    <?php } else { ?>
+        
+        <?php the_excerpt(); ?>
+        
+    <?php } ?>
 
-        <?php if(is_single( )) { ?>
-            <?php // get_template_part('template-parts/post/footer') ?>
-        <?php } ?>
+    <?php if(is_single( )) { ?>
+        <?php // get_template_part('template-parts/post/footer') ?>
+    <?php } ?>
 
+    <?php  if(!is_single()) { pnhuk_readmore_link(); } ?>
 
-        <?php if(!is_single()) {
-           _themename_readmore_link(); 
-           } ?>
-    </div>
 
 </article>
