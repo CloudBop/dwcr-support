66408ea6e9d588a65788675036a522b6fe484b8d
Cloudbop <boppincolin@gmail.com>
2021-04-14 01:56:17 +0100

    section5 see diff

--- template-parts/frontpage/frontpage-section5.php
+++ template-parts/frontpage/frontpage-section5.php
@@ -1,47 +1,39 @@
 <!-- Start Event Section -->
-<section id="event-section">
-  <div class="container p-0">
-    <div class="my-5 p-5 text-center event-wrapper">
-      <h2 class="display-4 mb-4">Next PNH Support Event</h2>
-      <p class="lead mb-5">
-        This is a countdown to our next PNH Support event
-      </p>
-      <div class="row mb-5">
-        <div class="col-sm-3">
-          00 <br />
-          Days
-        </div>
-        <div class="col-sm-3">
-          00 <br />
-          Hours
-        </div>
-        <div class="col-sm-3">
-          00 <br />
-          Minutes
-        </div>
-        <div class="col-sm-3">
-          00 <br />
-          Seconds
-        </div>
-      </div>
-      <p class="mb-5">
-        Subscribe to stay up to date with future events.
-      </p>
-      <div class="row">
-        <div class="col-md-6">
-          <input
-            type="email"
-            class="form-control form-control-lg event-input"
-            placeholder="Enter Email"
-          />
-        </div>
-        <div class="col-md-6">
-          <button class="btn pnh-red-button btn-block btn-lg">
-            <i class="fas fa-envelope-open-o"></i> Subscribe
-          </button>
+    <section id="event-section">
+      <div class="container p-none">
+        <div class="my-5 p-5 text-center event-wrapper">
+          <h2 class="mb-4">Next PNH Support Event</h2>
+          <p class="mb-5">This is a countdown to our next PNH Support event</p>
+          <div class="row mb-5">
+            <div class="col-3 col-md-3 event-number">
+              00 <br />
+              Days
+            </div>
+            <div class="col-3 col-md-3 event-number">
+              00 <br />
+              Hours
+            </div>
+            <div class="col-3 col-md-3 event-number">
+              00 <br />
+              Minutes
+            </div>
+            <div class="col-3 col-md-3 event-number">
+              00 <br />
+              Seconds
+            </div>
+          </div>
+          <p class="mb-5">Subscribe to stay up to date with future events.</p>
+          <div class="row">
+            <div class="col-md-6">
+            <input type="email" class="form-control form-control-lg event-input" placeholder="Enter Email">
+            </div>
+            <div class="col-md-6">
+              <button class="btn pnh-red-button btn-block btn-lg">
+                <i class="fas fa-envelope-open-o"></i> Subscribe
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-</section>
-<!-- End Event Section -->
+    </section>
+    <!-- End Event Section -->
