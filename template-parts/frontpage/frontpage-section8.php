deae575ccf665727897f1013e0ffd6cefd5946ce
Cloudbop <boppincolin@gmail.com>
2021-04-15 01:10:33 +0100

    fix frontpage-section8

--- template-parts/frontpage/frontpage-section8.php
+++ template-parts/frontpage/frontpage-section8.php
@@ -6,9 +6,9 @@
 
 
 
-    <a href="<?php get_the_field("social_section_1"); ?>" class="fab fa-facebook-square p-4"></a>
-    <a href="<?php get_the_field("social_section_2"); ?>" class="fab fa-twitter-square p-4"></a>
-    <a href="<?php get_the_field("social_section_3"); ?>" class="fab fa-instagram-square p-4"></a>
+    <a href="<?php the_field("social_section_1"); ?>" class="fab fa-facebook-square p-4"></a>
+    <a href="<?php the_field("social_section_2"); ?>" class="fab fa-twitter-square p-4"></a>
+    <a href="<?php the_field("social_section_3"); ?>" class="fab fa-instagram-square p-4"></a>
   </div>
 </section>
 <!-- End Social Section -->
