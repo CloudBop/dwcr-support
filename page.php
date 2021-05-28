9f90c67d24f5ec4eae1d9368468a9bfac0e937c3
Cloudbop <boppincolin@gmail.com>
2021-05-28 19:22:37 +0100

    + cmnt

--- page.php
+++ page.php
@@ -8,7 +8,8 @@
     <div class="row">
     <?php if(have_posts()) { ?>
       <?php while(have_posts()) { ?>
-        <?php the_post(); ?>
+        <?php // https://developer.wordpress.org/reference/functions/the_post/ 
+        the_post(); ?>
             <div class="col">
               <?php //get_template_part('template-parts/components/page/entry-header'); ?>
               <?php get_template_part('template-parts/components/page/entry-content'); ?>
