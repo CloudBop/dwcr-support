8b31a95756d92b0aa233e07f8cbd5e011dc2dbe2
Cloudbop <boppincolin@gmail.com>
2021-08-01 21:43:51 +0100

    +neatened +semantic <main/>

--- page.php
+++ page.php
@@ -1,50 +1,31 @@
 <?php get_header(); 
 // render Page, ie NOT post 
 ?>
-<main class="main-content">
   <?php get_template_part('template-parts/page/entry-header'); ?>
+  
   <div class="container">
     <div class="row">
       <?php if(have_posts()) { ?>
         <?php while(have_posts()) { ?>
           <?php // https://developer.wordpress.org/reference/functions/the_post/ 
-        the_post(); ?>
-            <div class="col-sm-8">
-              <?php //get_template_part('template-parts/components/page/entry-header'); ?>
-              <?php get_template_part('template-parts/page/entry-content'); ?>
-              <?php get_template_part('template-parts/page/entry-footer'); ?>
-              <small>page-template</small>
+            the_post(); ?>
+            <div class="col-sm-8 p-0">
+              <main class="main-content">
+                <?php //get_template_part('template-parts/components/page/entry-header'); ?>
+                <?php get_template_part('template-parts/page/entry-content'); ?>
+                <?php get_template_part('template-parts/page/entry-footer'); ?>
+                <!-- <small>page-template</small> -->
+              </main>
             </div>
 
-            <div class="col-sm-4">
+            <div class="col-sm-4 mt-3 pr-0 page-sidebar">
               <?php
-              if ( $post->post_parent ) {
-                  $children = wp_list_pages( array(
-                      'title_li' => '',
-                      'child_of' => $post->post_parent,
-                      'echo'     => 0
-                  ) );
-                  $title = get_the_title( $post->post_parent );
-              } else {
-                  $children = wp_list_pages( array(
-                      'title_li' => '',
-                      'child_of' => $post->ID,
-                      'echo'     => 0
-                  ) );
-                  $title = get_the_title( $post->ID );
-              }
-               
-              if ( $children ) : ?>
-                <div style="background-color: lavender;">
-                  <h2><?php echo $title; ?></h2>
-                  <ul>
-                      <?php echo $children; ?>
-                  </ul>
-                </div>
-              <?php endif; ?>
+                
+                // DONE:TODO add this as widget - left in case error.
+                // pnhuk_get_family_tree($post);
 
-              <?php dynamic_sidebar('sidebar-4') ?>
-
+                dynamic_sidebar('sidebar-page-children');
+              ?>                        
             </div>
           <?php 
           // if (comments_open() || get_comments_number() ) {
@@ -59,6 +40,4 @@
     <?php }  // end if else ?>
     </div>
   </div>
-</main>
-
 <?php get_footer(); ?> 
