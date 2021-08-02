08867f238f4e3c4aabd89a44995bf9fdb8538ec2
Cloudbop <boppincolin@gmail.com>
2021-08-02 17:08:03 +0100

    corrected to news_posts_sidebar +cleanup

--- search.php
+++ search.php
@@ -7,7 +7,7 @@
      </h1>
  </header>
   <div class="row">
-  <div class="col-sm-8">
+    <div class="col-sm-8">
     <?php if(have_posts()) : ?>
       <?php while(have_posts()) : the_post(); ?>
       <article class="card my-2 p-1">
@@ -22,21 +22,21 @@
             <?php get_template_part('template-parts/post/header') ?>  
             <?php the_excerpt(); ?>
             <?php pnhuk_readmore_link(); ?>
-          </article>
-          <?php endwhile; ?>
-        <?php echo paginate_links();?>
-      <?php else : ?>
-        <?php echo apautop('Sorry, no posts were found'); ?>
-        <?php endif; ?>
-      </div>
-      
-      
-      <aside class="col-sm-4 pr-0 page-sidebar">
-        <?php if(is_active_sidebar('page-sidebar')) : ?>
-          <?php dynamic_sidebar('page-sidebar'); ?>
-          <?php endif; ?>
-        </aside>
-      </div>
-      
+        </article>
+        <?php endwhile; ?>
+      <?php echo paginate_links();?>
+    <?php else : ?>
+      <?php echo apautop('Sorry, no posts were found'); ?>
+      <?php endif; ?>
     </div>
+      
+      
+    <aside class="col-sm-4 pr-0 page-sidebar">
+      <?php if(is_active_sidebar('news-posts-sidebar')) : ?>
+        <?php dynamic_sidebar('news-posts-sidebar'); ?>
+        <?php endif; ?>
+    </aside>
+    
+  </div>
+</div>
 <?php get_footer(); ?>
