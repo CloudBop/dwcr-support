c8952edb4fd360e4840357d884aaadb3f7ce63d6
Cloudbop <boppincolin@gmail.com>
2021-08-02 22:27:40 +0100

    neatened footer

--- footer.php
+++ footer.php
@@ -11,15 +11,15 @@
 <footer class="site-footer pb-5">
   <div class="container text-center">
     <div class="row py-5">
-      <div class="col-md-4 mb-3 mb-md-0">
+      <!-- <div class="col-md-4 mb-3 mb-md-0">
         <h3 class="mb-2">Spare Widget</h3>
         <a href="#" class="d-block footer-link">Help Center</a>
         <a href="#" class="d-block footer-link">PNH Forums</a>
         <a href="#" class="d-block footer-link">PNH Contact</a>
         <a href="#" class="d-block footer-link">Our site</a>
-      </div>
+      </div> -->
 
-      <div class="col-md-4 mb-3 mb-md-0">
+      <div class="col-md-8 mb-3 mb-md-0">
         <?php
         if (is_active_sidebar('sidebar-footer' ) ) {
         ?>
