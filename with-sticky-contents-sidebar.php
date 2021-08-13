4bdfc48d534a92a92a8dcce4cfd5ad1602be337e
Cloudbop <boppincolin@gmail.com>
2021-08-13 13:16:50 +0100

    reversed contents sidebar order. +cleanup

--- with-sticky-contents-sidebar.php
+++ with-sticky-contents-sidebar.php
@@ -10,29 +10,23 @@ get_header();
         <?php while(have_posts()) { ?>
           <?php // https://developer.wordpress.org/reference/functions/the_post/ 
             the_post(); ?>
-            <div class="col-sm-8 p-0">
-              <main class="main-content">
-                <?php //get_template_part('template-parts/components/page/entry-header'); ?>
-                <?php get_template_part('template-parts/page/entry-content'); ?>
-                <?php get_template_part('template-parts/page/entry-footer'); ?>
-                <!-- <small>page-template</small> -->
-              </main>
-            </div>
 
-            <aside class="col-sm-4 pr-0 page-sidebar">
-            
+
+            <aside class="col-sm-4 page-sidebar">
               <?php
                 dynamic_sidebar('contents-sidebar');
                 // echo do_shortcode("[toc]");
               ?>                        
-            
             </aside>
-          <?php 
-          // if (comments_open() || get_comments_number() ) {
-          //     comments_template();
-          // }
-          ?>
+
+            <div class="col-sm-8">
+              <main class="main-content">
+                <?php get_template_part('template-parts/page/entry-content'); ?>
+                <?php get_template_part('template-parts/page/entry-footer'); ?>
+              </main>
+            </div>
       <?php } // end while?>
+
     <?php } else {?>
         <div class="col">
           <?php get_template_part('template-parts/post/content','none'); ?>
