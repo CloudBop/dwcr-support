d6a7b9e2f33d176e2d02896145de07813ba57db2
Cloudbop <boppincolin@gmail.com>
2021-08-13 15:52:59 +0100

    updated frontpage

--- front-page.php
+++ front-page.php
@@ -8,12 +8,8 @@ get_header(); ?>
     get_template_part('template-parts/frontpage/frontpage','section1');
     get_template_part('template-parts/frontpage/frontpage','section2');
     get_template_part('template-parts/frontpage/frontpage','section3');
-
     get_template_part('template-parts/frontpage/frontpage','section4');
     get_template_part('template-parts/frontpage/frontpage','section5');
-    // get_template_part('template-parts/frontpage/frontpage','section6');
-    // get_template_part('template-parts/frontpage/frontpage','section7');
-    get_template_part('template-parts/frontpage/frontpage','section8');
   ?>
 </div>
 <?php get_footer(); ?>
