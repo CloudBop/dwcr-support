ece20f4cf9e76fe0f21bf4b11cb4b6b7e47a5c33
Cloudbop <boppincolin@gmail.com>
2021-08-15 00:26:31 +0100

    +404template

---
+++ 404.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+<div class="container">
+    <div class="row">
+        <main>
+            <div class="four-o-four">
+                <h1 class="display-1">404</h1>
+                <h2>Sorry, we can&rsquo;t find that</h2>
+                <p>Perhaps the page has changed or the the content is no longer relevent.<br>
+                Try returning to the <a href="/">homepage</a> to find what you are looking for.</p>
+            </div>
+        </main>
+    </div>
+</div>
+<?php get_footer(); 
+/**
+ * 
+ * <img src="<?php echo get_stylesheet_directory_uri().'/img/fh-404.png'; ?>" class="img-fluid" alt="404 broken-guitar-image">
+ */
+?>
