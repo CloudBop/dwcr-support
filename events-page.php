080048970f0bff4716ac14e11670d8c3b202f3e3
Cloudbop <boppincolin@gmail.com>
2021-08-15 22:29:54 +0100

    +cleanup events page

--- events-page.php
+++ events-page.php
@@ -11,32 +11,31 @@ get_header();
     </h1>   
   </header>
 </div>
-  
-  <div class="container mt-4">
-    <div class="row">
-      <?php if(have_posts()) { ?>
-        <?php while(have_posts()) { ?>
-          <?php // https://developer.wordpress.org/reference/functions/the_post/ 
-            the_post(); ?>
-            <div class="col-sm-12 ">
-              <main class="main-content">
-                <?php //get_template_part('template-parts/components/page/entry-header'); ?>
-                <?php get_template_part('template-parts/page/entry-content'); ?>
-                <?php get_template_part('template-parts/page/entry-footer'); ?>
-                <!-- <small>page-template</small> -->
-              </main>
-            </div>
-          <?php 
-          // if (comments_open() || get_comments_number() ) {
-          //     comments_template();
-          // }
-          ?>
-      <?php } // end while?>
-    <?php } else {?>
-        <div class="col">
-          <?php get_template_part('template-parts/post/content','none'); ?>
-        </div>
-    <?php }  // end if else ?>
-    </div>
+
+<div class="container-fluid mt-4">
+  <div class="row">
+    <?php if(have_posts()) { ?>
+      <?php while(have_posts()) { ?>
+        <?php // https://developer.wordpress.org/reference/functions/the_post/ 
+          the_post(); ?>
+          <div class="col-12">
+            <main class="main-content">
+              <article class="entry-content" <?php post_class(); ?>>
+                  <?php the_content()  // EventsList Custom Plugin mkup ?>
+                </article> 
+            </main>
+          </div>
+        <?php 
+        // if (comments_open() || get_comments_number() ) {
+        //     comments_template();
+        // }
+        ?>
+    <?php } // end while?>
+  <?php } else {?>
+      <div class="col">
+        <?php get_template_part('template-parts/post/content','none'); ?>
+      </div>
+  <?php }  // end if else ?>
   </div>
+</div>
 <?php get_footer(); ?> 
