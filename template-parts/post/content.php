3ab32242ac9bafdf70baf7dd4d6f423df0e853b8
Cloudbop <boppincolin@gmail.com>
2021-08-15 02:34:03 +0100

    +single post content styling

--- template-parts/post/content.php
+++ template-parts/post/content.php
@@ -1,4 +1,4 @@
-<article <?php echo post_class('card');?> >
+<article <?php echo post_class('bg-white');?> >
 
     <?php if(get_the_post_thumbnail() !== '') { ?>
         <div class="">
@@ -8,13 +8,13 @@
         </div>
     <?php } ?>
 
-    <div class="main-text-area">
+    <div class="p-1">
+
         <?php get_template_part('template-parts/post/header') ?>
+    
         <?php if(is_single()) { ?>
             <?php 
                 the_content(); 
-                
-                // for multi-page post or pages
                 wp_link_pages();
             ?>
         <?php } else { ?>
@@ -22,18 +22,16 @@
         <?php } ?>
 
         <?php if(is_single( )) { ?>
-            <ul>
-                <li class="previous"><?php previous_post_link( '%link', '&larr; %title' ); ?></li>
-                <li class="next"><?php next_post_link( '%link', '%title &rarr;' ); ?></li>
-            </ul>
             <?php  get_template_part('template-parts/post/footer') ?>
         <?php } ?>
 
         <?php  if(!is_single()) { pnhuk_readmore_link(); } ?>
 
         <?php
-            // echo ":  ".is_page();
-            // echo ":   ".is_single();
+            echo ":  ".is_page();
+            echo ":   ".is_single();
         ?>
+
     </div>
+
 </article>
