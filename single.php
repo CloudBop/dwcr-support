d792a5f49c2a82987d89001000637c581d3403bc
Cloudbop <boppincolin@gmail.com>
2021-08-15 22:35:31 +0100

    cleanup single

--- single.php
+++ single.php
@@ -4,10 +4,35 @@
 
 <div class="container mt-3">
     <div class="row">
-        <main class="col-sm-8 main-content">
-            <?php get_template_part( 'wp-loop/loop', 'single' ); ?>
-        </main>
-        <aside class="col-sm-4 sidebar-content">
+        <div class="col-md-8 p-sm-0 pr-md-2 main-content">
+            <main>
+            <?php if(have_posts()) { ?>
+                <?php while(have_posts()) { ?>
+                    <?php the_post(); ?>
+
+                    <?php  get_template_part('template-parts/post/content', get_post_format()); ?>
+                    
+                    <?php 
+                    //   if (get_theme_mod('_themename_display_author_info', true)) {
+                    //     //   get_template_part('template-parts/single/author');
+                    //   } 
+                        
+                    // prev || next links
+                    // get_template_part('template-parts/single/navigation');
+                    
+                    // if comments are open || comments exist
+                    //   if (comments_open() || get_comments_number() ) {
+                    //       comments_template();
+                    //   }
+                //  end while       
+                ?>
+                <?php } ?>
+                <?php } else { ?>
+                    <?php get_template_part('template-parts/post/content','none'); ?>
+                <?php } ?>
+                </main>
+        </div>
+        <aside class="col-sm-4 p-md-0">
             <?php dynamic_sidebar('news-posts-sidebar'); ?>
         </aside>
     </div>
