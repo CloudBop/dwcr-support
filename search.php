3550855511df108f91427ae5880316a87c3c5b33
Cloudbop <boppincolin@gmail.com>
2021-08-15 22:32:46 +0100

    cleanup search

--- search.php
+++ search.php
@@ -1,16 +1,17 @@
 <?php get_header(); ?>
 
-<div class="container content">
-  <header class="">
-     <h1>
-       <?php printf( __( 'Search: %s', 'dwcr-support' ), '<span>' . esc_html( get_search_query() ) . '</span>' ); ?>
-     </h1>
- </header>
-  <div class="row">
-    <div class="col-sm-8">
+  <div class="container content mt-3">
+    <div class="row">
+      <div class="col-md-8 p-sm-0 pr-md-2">
+      <header class="card p-3">
+        <h1>
+          <?php printf( __( 'Search: %s', 'dwcr-support' ), '<span>' . esc_html( get_search_query() ) . '</span>' ); ?>
+        </h1>
+      </header>
+    
     <?php if(have_posts()) : ?>
       <?php while(have_posts()) : the_post(); ?>
-      <article class="card my-2 p-1">
+      <article class="card my-2">
         <?php if(get_the_post_thumbnail() !== '') { ?>
             <div class="img-fluid">
               <?php 
@@ -18,20 +19,36 @@
               the_post_thumbnail( 'pnhuk-theme-blog-image' ); ?>
             </div>
             <?php } ?>
-            
-            <?php get_template_part('template-parts/post/header') ?>  
-            <?php the_excerpt(); ?>
-            <?php pnhuk_readmore_link(); ?>
+                    
+            <header class="p-2">
+                <?php if (is_single()) { ?>
+                    <h1> <?php the_title() ?> </h1>
+                <?php } else { ?>
+                    <h2>
+                        <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title() ?>
+                        </a>
+                    </h2>
+                <?php } ?>
+                <div>
+                    <?php _themename_post_meta(); ?>
+                </div>
+                <?php the_excerpt(); ?>
+                <?php pnhuk_readmore_link(); ?>
+            </header>
+
         </article>
+        
         <?php endwhile; ?>
-      <?php echo paginate_links();?>
-    <?php else : ?>
+
+        <?php echo paginate_links();?>
+ 
+      <?php else : ?>
       <?php echo apautop('Sorry, no posts were found'); ?>
       <?php endif; ?>
     </div>
       
       
-    <aside class="col-sm-4 pr-0 page-sidebar">
+    <aside class="col-md-4 pr-sm-0">
       <?php if(is_active_sidebar('news-posts-sidebar')) : ?>
         <?php dynamic_sidebar('news-posts-sidebar'); ?>
         <?php endif; ?>
